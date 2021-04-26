13bb6b3d6f687b4c328e9ddafaaad10c36b3cfa5
Alexandrea Whitehouse <AWhitehouse@student.neumont.edu>
2021-04-26 15:11:37 -0600

    finished editor assignment

--- Assets/Scripts/Common/Utilities.cs
+++ Assets/Scripts/Common/Utilities.cs
@@ -9,7 +9,7 @@ public static class Utilities
         if(point.x > max.x) point.x = min.x;
         if(point.x < min.x) point.x = max.x;
         if(point.y > max.y) point.y = min.y;
-        if(point.y < min.y) point.y = min.y;
+        if(point.y < min.y) point.y = max.y;
 
         return point;
     }
